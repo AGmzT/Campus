96c3258ed320e566efdc145843b3d516f8605087
Alvaro Gomez <agomez_t@hotmail.com>
2022-08-22 20:19:36 -0300

    ejercicio_07_1
    ejercicio_07_1

---
+++ ejercicio_07_1/main.py
@@ -0,0 +1,8 @@
+import operaciones
+
+op = operaciones.operaciones()
+
+print(op.sumar(1, 10))
+print(op.restar(1, 10))
+print(op.multip(1, 10))
+print(op.div(1, 10))
---
+++ ejercicio_07_1/operaciones.py
@@ -0,0 +1,30 @@
+# modulo con las operaciones basicas de una calculadora
+
+class operaciones():
+    
+    def sumar(self, a , b):
+        
+        """devuelve la suma de a + b"""
+        
+        return a + b
+    
+    def restar(self, a , b):
+        
+        """devuelve la suma de a - b"""
+        
+        return a - b
+    
+    def multip(self, a , b):
+        
+        """devuelve la multiplicación de a por b"""
+        
+        return a * b
+    
+    def div(self, a , b):
+        
+        """devuelve la divición de a entre b"""
+        
+        return a / b
+
+
+
