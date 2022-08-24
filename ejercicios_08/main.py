6ac10b91222b7d8af9587f8250343734bd851bbe
Alvaro Gomez <agomez_t@hotmail.com>
2022-08-24 05:52:04 -0300

    'Ejercicios del tema 8'

---
+++ ejercicios_08/main.py
@@ -0,0 +1,13 @@
+"""Ejercios del tema 8"""
+from registro import registro as reg
+
+def main():
+    op = input('Elige un ejercicio a realizar\n Ejercicio 1: 1\n Ejercicio 2: 2\n')
+    if op == '1':
+        e = reg
+    elif op == 2:
+        pass
+    
+
+if __name__ == '__main__':
+    main()
---
+++ ejercicios_08/registro.py
@@ -0,0 +1,39 @@
+from datetime import datetime as dt
+
+class registro():
+    """Accede a un archivo para leer y registrar la hora de entrada de una persona"""
+    
+    _archivo ='ejercicios_08\entrada.txt'
+    
+    def __init__(self):
+        
+        opcion = None
+        while opcion != '3':
+            opcion =input('Buenos días!\n Menu:\n Leer el registro - 1\n Agregar entrada - 2\n Salir - 3\n')
+            match opcion:
+                case '1':
+                    self.leerRegistro()
+                case '2':
+                    self.agregarRegistro()
+                case '3':
+                    print('Hasta luego')
+                case '':
+                    print('Ingersa 1, 2, o 3')
+
+    def leerRegistro(self):
+        r = open(self._archivo, 'r' )
+        lineas = r.readlines()
+        r.close()
+        
+        for linea in lineas:
+            print(linea)
+    
+    def agregarRegistro(self):
+        
+        nombre = input('ingrese el nombre:\n')
+        fecha = dt.strftime((dt.now() ),'%Y:%m:%d: %H:%M')
+        linea = nombre + ' -> ' + fecha + '\n'
+        r = open(self._archivo, 'a')
+        r.writelines(linea)
+        r.close()
+
