122d3665300ece60f7340fd4bfec2ef195c80872
Alvaro Gomez <agomez_t@hotmail.com>
2022-09-08 19:21:35 -0300

    ejercicios_10

--- ejercicios_10/contadorDias.py
+++ ejercicios_10/contadorDias.py
@@ -1,37 +1,38 @@
-import tkinter
+import tkinter as tk
 from datetime import datetime as dt
 from datetime import timedelta as td
 from tkinter.font import Font
 
-class contador():
-    def dias():
+class contador(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        
+        #tamaño de la ventana
+        self.geometry('200x200')
+        #ventana sin titulo
+        self.title('Contador de días')
+        #ventana transparente
+        self.attributes('-alpha', 0.5)
+        #sin icono en la barra de titulo
+        self.attributes('-toolwindow', True)
+        
         hoy = dt.now( )
         inicio = dt.strptime(('2022:05:19'),'%Y:%m:%d')
         dif = int((hoy - inicio) / td(days=1))
-        return dif
+       
+        #fuente a usar
+        fuente1 = Font(family='Comic Sans Ms', size= 25,slant= 'roman', weight='bold')
+        fuente2 = Font(family='Comic Sans Ms', size= 50,slant= 'roman', weight='bold')
 
-    dias = dias()    
-    ventana = tkinter.Tk()
-    #tamaño de la ventana
-    ventana.geometry('200x200')
-    #ventana sin titulo
-    ventana.title('')
-    #ventana transparente
-    ventana.attributes('-alpha', 0.5)
-    #sin icno en la barra de titulo
-    ventana.attributes('-toolwindow', True)
+        #etiquetas
+        dia_label1 = tk.Label(self, text = 'DÍAS', bg = 'white', fg= 'green', font= fuente1)
+        dia_label1.pack(ipadx=50, ipady= 10, fill= 'both')
 
-    #fuente a usar
-    fuente1 = Font(family='Comic Sans Ms', size= 25,slant= 'roman', weight='bold')
-    fuente2 = Font(family='Comic Sans Ms', size= 50,slant= 'roman', weight='bold')
+        dia_label2 = tk.Label(self, text = dif, bg = 'white', fg= 'green', font = fuente2)
+        dia_label2.pack(ipadx=50, ipady= 50, fill= 'both')
+    
 
-    #etiquetas
-    dia_label1 = tkinter.Label(ventana, text = 'DÍAS', bg = 'white', fg= 'green', font= fuente1)
-    dia_label1.pack(ipadx=50, ipady= 10, fill= 'both')
-
-    dia_label2 = tkinter.Label(ventana, text = dias, bg = 'white', fg= 'green', font = fuente2)
-    dia_label2.pack(ipadx=50, ipady= 50, fill= 'both')
-
-
-    ventana.mainloop()
-
+if __name__ == '__main__':
+    cont = contador()
+    cont.mainloop()
